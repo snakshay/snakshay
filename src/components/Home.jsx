50a26b927233f6ee75c4830e42c6cb51f55c6f4e
Nair, Akshay (ax8401) <ax8401@att.com>
2023-07-05 20:10:55 +0530

    profile photo updated

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -5,7 +5,7 @@ import ReactGA from 'react-ga';
 import FadeIn from '../animation/FadeIn';
 import {data} from '../data'
 import resume from '../documents/Akshay Nair.pdf'
-import profile from '../images/akshay.jpeg'
+import profile from '../images/Akshay.jpg'
 
 const Home = () => {
 
